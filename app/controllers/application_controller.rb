e6369a8e6b8f61476e144107776dffa4947fe369
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-19 08:07:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
     
     post '/teams' do 
       
-      @team_name = params[:team]
+      @team_name = params[:team[name]]
+      @team_motto= params[:team[motto]]
       
       
       erb : '../views/team'
